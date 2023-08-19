650b086d73028aa92fdce75bdefb7a178de615ef
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-08-19 15:28:59 +0000

    Deploying to gh-pages from @ nastyaosetrova8/rent-a-car@1a0d85bc1dee780d5ea2903e4ac5185e8616a479 🚀

---
+++ static/js/126.d5fe3aa0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrent_a_car=self.webpackChunkrent_a_car||[]).push([[126],{9126:function(a,t,r){r.d(t,{KMP:function(){return e}});var n=r(9983);function e(a){return(0,n.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"}}]})(a)}}}]);
+//# sourceMappingURL=126.d5fe3aa0.chunk.js.map
